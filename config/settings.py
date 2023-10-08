a1dc732ac71fc280e36352ddcc1bb94c64af5093
updaun <updauney@daum.net>
2023-10-08 20:39:45 +0900

    [feat] Django Server Setting
    - secret key 비식별화
    - add requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -59,8 +59,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
