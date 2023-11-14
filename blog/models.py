b1f87b3428fbb5acd3e5359fe3b715dd49949d05
dage8044 <dage8044@gmail.com>
2023-11-14 20:02:02 +0900

    [feat] category, blog model

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True) # image web url
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="blogs")
+    view = models.PositiveIntegerField(default=0)
+    category = models.ManyToManyField('category.Category', related_name='blogs')
+    hidden = models.BooleanField(default=False)
+
+
