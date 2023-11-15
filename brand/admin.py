0a52d8c07a1f8bc852f71cc667db6423fa50200b
updaun <updauney@daum.net>
2023-11-15 23:50:03 +0900

    [feat] brand, product frontend

--- brand/admin.py
+++ brand/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Brand
 
-# Register your models here.
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    pass
--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
