8e94a51031841c7fbe28596c659306707dd1ef75
updaun <updauney@daum.net>
2023-11-18 19:56:46 +0900

    [feat] blog model update, blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+# admin.site.register(Blog)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "user",
+        "view",
+    )
--- blog/serializers.py
+++ blog/serializers.py
@@ -4,12 +4,15 @@ from category.models import Category
 from category.serializers import CategorySerializer
 
 
+# 생성용 blog serializer
 class BlogSerializer(serializers.ModelSerializer):
-    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True, required=False)
+    # category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True, required=False)
     class Meta:
         model = Blog
         fields = ("id", "title", "content", "image", "user", "category")
 
+
+# 읽기용 blog serializer
 class BlogReadSerializer(serializers.ModelSerializer):
     category = CategorySerializer(many=True, read_only=True)
     class Meta:
