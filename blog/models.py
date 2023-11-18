b839a9a289760bbbc239f65e60de4938bdeb7b1f
updaun <updauney@daum.net>
2023-11-18 20:42:50 +0900

    [feat] custom user model

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(null=True, blank=True)
     image = models.CharField(max_length=200, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="blogs")
+    user = models.ForeignKey("users.User", on_delete=models.SET_NULL, null=True, blank=True, related_name="blogs")
     view = models.PositiveIntegerField(default=0)
     category = models.ManyToManyField('category.Category', related_name='blogs', blank=True)
     hidden = models.BooleanField(default=False)
