87b56c3491243a9180d209ddcbca30e12c086665
kmgyu <lidersy961@gmail.com>
2023-11-21 20:19:00 +0900

    [deploy] settings separate

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,29 +9,20 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os, environ
+import os
 from pathlib import Path
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 CUSTOM_APPS = [
     'todo',
@@ -89,12 +80,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -147,14 +133,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS":"api.pagination.CustomPageNumberPagination",
-    "PAGE_SIZE":10,
-    "DEFAULT_AUTHENTICATION_CLASSES":[
-        # "rest_framework.authentication.SessionAuthentication",
-        "api.session_authentication.NoCsrfSessionAuthentication",
-    ],
-}
+
 
 MY_NAME = "김민규"
 
---
+++ config/settings/local.py
@@ -0,0 +1,31 @@
+from .base import *
+import os, environ
+from pathlib import Path
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES":[
+        # "rest_framework.authentication.SessionAuthentication",
+        "api.session_authentication.NoCsrfSessionAuthentication",
+    ],
+    "DEFAULT_PAGINATION_CLASS":"api.pagination.CustomPageNumberPagination",
+    "PAGE_SIZE":10,
+}
