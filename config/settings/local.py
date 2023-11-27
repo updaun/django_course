baf5b8ec5a3afc6034d6d7670ff9851935a3f0d1
kmgyu <lidersy961@gmail.com>
2023-11-27 20:51:18 +0900

    awshzdngyrskm

--- config/settings/local.py
+++ config/settings/local.py
@@ -9,7 +9,7 @@ env = environ.Env(
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-7!t$i*_145_k-+dsji2j5vuwx&s880)=br*__rxvz5cjyh%2('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
